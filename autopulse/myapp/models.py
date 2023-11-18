0ce70777e73ba348545f19bd184a2dd2c78d6494
Mrudul <mrudul888@gmail.com>
2023-11-18 17:27:48 +0530

    done database connectivity

--- autopulse/myapp/models.py
+++ autopulse/myapp/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=6)
+    contact = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+
+
+class Login(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+
+
+class myadmin(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+
+
+class Technician(models.Model):
+    id = models.AutoField(primary_key=True)
+    tech_name = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=6)
+    contact = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+
+
+class Shopkeeper(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner_name = models.CharField(max_length=100)
+    shop_name = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=6)
+    contact = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    opent = models.TimeField()
+    closet = models.TimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
--- autopulse/myapp/views.py
+++ autopulse/myapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+from django.shortcuts import render, redirect
+from .models import User, Technician, myadmin, Shopkeeper, Login
+from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from datetime import datetime
 from django.db.models import Max
@@ -15,19 +18,85 @@ def about(request):
 
 
 def admin_login(request):
-    return render(request, 'admin_login.html')
+    if request.method == 'POST':
+        username = "admin"
+        password = "admin"
+        try:
+            admin = myadmin.objects.get(username=username, password=password)
+            return redirect('/admin_home')
+        #  Perform necessary actions after successful login
+        # return redirect('/')
+        except myadmin.DoesNotExist:
+            context = {'msg': 'Invalid username or password'}
+            return redirect('/admin_login', context)
+
+    else:
+        return render(request, 'admin_login.html')
 
 
 def admin_home(request):
-    return render(request, 'admin_home.html')
+       return render(request, 'admin_home.html')
 
 
 def shopkeeper_registration(request):
-    return render(request, 'shopkeeper_registration.html')
+    if request.method == 'POST':
+        owner_name = request.POST['mname']
+        shop_name = request.POST['shname']
+        place = request.POST['place']
+        city = request.POST['city']
+        district = request.POST['dist']
+        pincode = request.POST['pincode']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        latitude = request.POST['latitude']
+        longitude = request.POST['longitude']
+        opent = request.POST['opent']
+        closet = request.POST['closet']
+        username = request.POST['sname']
+        password = request.POST['password']
+
+        # Create a new Customer object and save it to the database
+        new_shopkeeper = Shopkeeper(
+            owner_name=owner_name,
+            shop_name=shop_name,
+            place=place,
+            city=city,
+            district=district,
+            pincode=pincode,
+            contact=contact,
+            email=email,
+            latitude=latitude,
+            longitude=longitude,
+            opent=opent,
+            closet=closet,
+            username=username,
+            password=password  # Note: Password should be securely hashed before saving to the database
+        )
+        new_shopkeeper.save()
+        messages.success(request, 'You have been successfully registered!')
+        return redirect('/shopkeeper_login')
+    else:
+
+     return render(request, 'shopkeeper_registration.html')
 
 
 def shopkeeper_login(request):
-    return render(request, 'shopkeeper_login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Check if the username and password match the database records
+        try:
+            shopkeeper = Shopkeeper.objects.get(username=username, password=password)
+            messages.success(request, 'Login successful!')
+            return redirect('/medicalshop_home')
+            # Perform necessary actions after successful login
+            return redirect('/')
+        except Dealer.DoesNotExist:
+            messages.error(request, 'Invalid credentials. Please try again.')
+            return redirect('/medicalshop_login')
+    else:
+     return render(request, 'shopkeeper_login.html')
 
 
 def shopkeeper_home(request):
@@ -35,11 +104,66 @@ def shopkeeper_home(request):
 
 
 def tech_registration(request):
-    return render(request, 'tech_registration.html')
+    if request.method == 'POST':
+        tech_name = request.POST['tname']
+        place = request.POST['place']
+        city = request.POST['city']
+        district = request.POST['dist']
+        pincode = request.POST['pincode']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        username = request.POST['dsname']
+        password = request.POST['password']
+
+        # Create a new Customer object and save it to the database
+        new_technician = Technician(
+            tech_name=tech_name,
+            place=place,
+            city=city,
+            district=district,
+            pincode=pincode,
+            contact=contact,
+            email=email,
+            username=username,
+            password=password  # Note: Password should be securely hashed before saving to the database
+        )
+        new_technician.save()
+        # user1 = User.objects.create_user(first_name=first_name,
+        #                                  last_name=last_name,
+        #                                  gender=gender,
+        #                                  place=place,
+        #                                  city=city,
+        #                                  district=district,
+        #                                  pincode=pincode,
+        #                                  contact=contact,
+        #                                  email=email,
+        #                                  username=username,
+        #                                  password=password)
+        #
+        # user1.save()
+        messages.success(request, 'You have been successfully registered!')
+        return redirect('/technician_login')
+    else:
+     return render(request, 'tech_registration.html')
 
 
 def tech_login(request):
-    return render(request, 'tech_login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Check if the username and password match the database records
+        try:
+            technician = Technician.objects.get(username=username, password=password)
+            messages.success(request, 'Login successful!')
+            return redirect('/technician_home')
+            # Perform necessary actions after successful login
+            return redirect('/')
+        except Technician.DoesNotExist:
+            messages.error(request, 'Invalid credentials. Please try again.')
+            return redirect('/technician_login')
+    else:
+     return render(request, 'tech_login.html')
 
 
 def tech_home(request):
@@ -47,11 +171,67 @@ def tech_home(request):
 
 
 def user_registration(request):
-    return render(request, 'user_registration.html')
+    if request.method == 'POST':
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        place = request.POST['place']
+        city = request.POST['city']
+        district = request.POST['dist']
+        pincode = request.POST['pincode']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        username = request.POST['usname']
+        password = request.POST['password']
+
+        # Create a new Customer object and save it to the database
+        new_user = User(
+            first_name=first_name,
+            last_name=last_name,
+            place=place,
+            city=city,
+            district=district,
+            pincode=pincode,
+            contact=contact,
+            email=email,
+            username=username,
+            password=password  # Note: Password should be securely hashed before saving to the database
+        )
+        new_user.save()
+        # user1 = User.objects.create_user(first_name=first_name,
+        #                                  last_name=last_name,
+        #                                  gender=gender,
+        #                                  place=place,
+        #                                  city=city,
+        #                                  district=district,
+        #                                  pincode=pincode,
+        #                                  contact=contact,
+        #                                  email=email,
+        #                                  username=username,
+        #                                  password=password)
+        #
+        # user1.save()
+
+        return redirect('/user_login')
+    else:
+     return render(request, 'user_registration.html')
 
 
 def user_login(request):
-    return render(request, 'user_login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Check if the username and password match the database records
+        try:
+            user = User.objects.get(username=username, password=password)
+            return redirect('/user_home')
+            # Perform necessary actions after successful login
+            # return redirect('/')
+        except:
+            context = {'msg': 'Invalid username or password'}
+            return redirect('/user_login', context)
+    else:
+     return render(request, 'user_login.html')
 
 
 def user_home(request):
